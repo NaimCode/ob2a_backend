d8a7a8564309764b696fecea6eb8cf7e9f406af3
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-05-29 21:07:54 -0700

    firebaseupload

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,56 @@
-module.exports=({env})=>{
-    if(env('NODE_ENV')==='production'){
+"use strict";
+
+/**
+ * Module dependencies
+ */
+
+const admin = require("firebase-admin");
+var serviceAccount = require("./ob2astorage-firebase-adminsdk-7yrbx-b73c1da095.json");
+module.exports = {
+  init(config) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: "gs://ob2astorage.appspot.com",
+    });
+    const bucket = "gs://ob2astorage.appspot.com";
+
     return {
-    upload:{
-        provider:'aws-s3',
-        providerOption:{
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey:env('AWS_ACCESS_SECRET'),
-            region:env('AWS_REGION'),
-            params:{
-                Bucket:env('AWS_BUCKET')
+      upload(file) {
+        return new Promise((resolve, reject) => {
+          const path = file.path ? `${file.path}/` : "";
+          const filename = `${path}${file.hash}${file.ext}`;
+          const buff = Buffer.from(file.buffer, "binary");
+          const remoteFile = bucket.file(filename);
+          remoteFile.save(
+            buff,
+            {
+              resumable: false,
+              contentType: file.mime,
+              public: true,
+            },
+            (err) => {
+              if (err) {
+                reject(err);
+              }
+              file.url = `https://storage.googleapis.com/${config.bucket}/${filename}`;
+              resolve();
             }
-        }
-    }
-}
-return {
-    
-}
-}}
+          );
+        });
+      },
+      delete(file) {
+        return new Promise((resolve, reject) => {
+          const path = file.path ? `${file.path}/` : "";
+          const filename = `${path}${file.hash}${file.ext}`;
+          const remoteFile = bucket.file(filename);
+          remoteFile.delete((err, _) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve();
+          });
+        });
+      },
+    };
+  },
+};
