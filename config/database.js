7c07bca1a66ce6518aa862c0418f0e0cf1d1a1db
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-05-30 19:42:41 -0700

    firebaseupload 2323

--- config/database.js
+++ config/database.js
@@ -1,19 +1,43 @@
-const parse =require('pg-connection-string').parse;
+const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
 
-  return {
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    }
+  }
+  
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-}};
+  }
+};
