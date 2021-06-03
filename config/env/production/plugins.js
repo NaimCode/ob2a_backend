1ef8a23d82240b9bc90b5da01614e11f347480c6
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-06-03 14:57:36 -0700

    test without env

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,7 +4,7 @@ module.exports=({env})=>({
         providerOption:{
             accessKeyId: 'AKIA6AA2F76S5T4GS64P',
             secretAccessKey:'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',
-            region:'UE (Paris) eu-west-3',
+            region:'eu-west-3',
             params:{
                 Bucket:'ob2a'
             }
