27ac9734bb5b96d25c60c7d365b921a5ad6b2d5b
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-06-03 15:13:22 -0700

    test without env

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,11 +2,11 @@ module.exports=({env})=>({
     upload:{
         provider:'aws-s3',
         providerOption:{
-            accessKeyId: 'AKIA6AA2F76S5T4GS64P',
-            secretAccessKey:'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',
-            region:'eu-west-3',
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey:env('AWS_ACCESS_SECRET'),
+            region:env('AWS_REGION'),
             params:{
-                Bucket:'ob2a'
+                Bucket:env('AWS_BUCKET')
             }
         }
     }
