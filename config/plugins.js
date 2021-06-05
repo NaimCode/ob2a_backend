ceb213cf2841eb44b0e2f8750507ab845f313f01
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-06-05 10:23:27 -0700

    test cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,12 @@
-module.exports=({env})=>({
-    upload:{
-        provider:'aws-s3',
-        providerOption:{
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey:env('AWS_ACCESS_SECRET'),
-            region:env('AWS_REGION'),
-            params:{
-                Bucket:env('AWS_BUCKET')
-            }
-        }
-    }
-})
+module.exports = ({ env }) => ({
+  
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  
+  });
